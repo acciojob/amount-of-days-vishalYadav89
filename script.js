22657f392e711f3258b33fa0da81b01beeafbcfb
Vishal Yadav <ydvishal89@gmail.com>
2026-02-17 06:50:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 function daysOfAYear(year){
 	//for leap year
-	if((year%4==0)||(year%400==0 && year %100 != 0)){
+	if((year%4===0 &&year %100 !== 0)||(year%400===0 )){
 		return 366;
 	}
 	else{
